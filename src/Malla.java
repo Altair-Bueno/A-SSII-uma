cafed8cf7edaf9b5e0179a068da81ea6b95e0b95
Altair-Bueno <altair.bueno@uma.es>
2021-03-16 11:55:31 +0100

    Malla.java created

---
+++ src/Malla.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Malla {
+    Random seed;
+    int [][] malla;
+
+    public Malla (int seed, int filas , int columnas , int nObstaculos) {
+
+    }
+
+    public String ver() {
+        return null;
+    }
+
+    public int[][] getMalla() {
+        return malla;
+    }
+
+    @Override
+    public String toString() {
+        return ver();
+    }
+}
