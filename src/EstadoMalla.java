0a5c2a66352cba8fc9808900bdad5677cdf758e9
Altair-Bueno <altair.bueno@uma.es>
2021-03-18 08:58:22 +0100

    Added 'hito2' files

---
+++ src/EstadoMalla.java
@@ -0,0 +1,19 @@
+package practica2021a;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author L.Mandow 
+ * @date  2021-03-11
+ * 
+ * Plantilla para representar los estados de la práctica
+ *
+ */
+
+public class EstadoMalla implements Estado {
+	
+
+
+		
+}
