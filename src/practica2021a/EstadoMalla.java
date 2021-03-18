576931ebd03bc856693ee715908deab171133cc5
Altair-Bueno <altair.bueno@uma.es>
2021-03-18 09:02:09 +0100

    Added 'hito2' files

--- src/practica2021a/EstadoMalla.java
+++ src/practica2021a/EstadoMalla.java
@@ -12,8 +12,25 @@ import java.util.List;
  */
 
 public class EstadoMalla implements Estado {
-	
 
 
-		
+    @Override
+    public List<? extends Estado> calculaSucesores() {
+        return null;
+    }
+
+    @Override
+    public int coste(Estado e2) {
+        return 0;
+    }
+
+    @Override
+    public int h(Estado objetivo) {
+        return 0;
+    }
+
+    @Override
+    public void ver() {
+
+    }
 }
