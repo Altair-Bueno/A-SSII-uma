06623115f02de205348b8845ebc2d6729f6e8226
Altair-Bueno <altair.bueno@uma.es>
2021-03-18 10:08:48 +0100

    Added EstadoMalla.h

--- src/practica2021a/EstadoMalla.java
+++ src/practica2021a/EstadoMalla.java
@@ -11,10 +11,10 @@ import java.util.Objects;
  */
 
 public class EstadoMalla implements Estado {
-    int fila;
-    int columna;
+    private final int fila;
+    private final int columna;
 
-    Malla malla;
+    private Malla malla;
 
     final int[][] matrizObstaculos;
 
@@ -50,12 +50,24 @@ public class EstadoMalla implements Estado {
 
     @Override
     public int h(Estado objetivo) {
-        //int distanciaManhattan = (this.fila - objetivo) + ()
-        return 0;//distanciaManhattan;
+        if (objetivo instanceof EstadoMalla) {
+            EstadoMalla estadoMalla = (EstadoMalla) objetivo;
+            return Math.abs(estadoMalla.getColumna() - columna) + Math.abs(estadoMalla.getFila() - fila);
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 
     @Override
     public void ver() {
 
+    }
+
+    public int getFila() {
+        return fila;
+    }
+
+    public int getColumna() {
+        return columna;
     }
 }
