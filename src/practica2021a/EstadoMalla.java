f6aa757d44bde094e9eaab24be03875c06397971
jxtaaa <jlbp@uma.es>
2021-03-18 09:44:08 +0100

    Actualizacion EstadoMalla.java

--- src/practica2021a/EstadoMalla.java
+++ src/practica2021a/EstadoMalla.java
@@ -38,7 +38,9 @@ public class EstadoMalla implements Estado {
     @Override
     public List<? extends Estado> calculaSucesores() {
         List<? extends Estado> sucs = new ArrayList<>();
+        if(this.fila + 1 > 3){
 
+        }
     }
 
     @Override
