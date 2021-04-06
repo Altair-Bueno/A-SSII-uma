c0ac299726e16d60099c7dee66a268213b91f1c8
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2021-04-06 11:00:55 +0200

    Added AbiertosList and AbiertosPriorityQueue

---
+++ src/practica2021a/AbiertosList.java
@@ -0,0 +1,33 @@
+package practica2021a;
+
+public class AbiertosList <E extends Estado> extends Abiertos<E>{
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void offer(int f, E e) {
+
+    }
+
+    @Override
+    public E poll() {
+        return null;
+    }
+
+    @Override
+    public void remove(E e) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void ver() {
+
+    }
+}
---
+++ src/practica2021a/AbiertosPriorityQueue.java
@@ -0,0 +1,34 @@
+package practica2021a;
+
+public class AbiertosPriorityQueue <E extends Estado> extends Abiertos<E> {
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void offer(int f, Estado estado) {
+
+    }
+
+    @Override
+    public E poll() {
+        return null;
+    }
+
+    @Override
+    public void remove(Estado estado) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void ver() {
+
+    }
+}
