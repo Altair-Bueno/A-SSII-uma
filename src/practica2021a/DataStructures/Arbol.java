1b214ff7de19e2b99e8fc43e76f56e7d7f6a7fee
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2021-04-08 08:55:14 +0200

    Added nodo

---
+++ src/practica2021a/DataStructures/Arbol.java
@@ -0,0 +1,14 @@
+package practica2021a.DataStructures;
+
+import practica2021a.Estado;
+
+public abstract class Arbol<E extends Estado> {
+	
+	public abstract void put(Nodo nodo);
+	
+	public abstract boolean containsKey(E estado);
+	
+	public abstract Nodo get(E estado);
+	
+	public abstract void ver();
+}
